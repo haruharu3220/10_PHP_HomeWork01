893126cc7087e1d428ed01083f658c66203153a9
Hayato <hayato.314.nico@gmail.com>
2023-01-22 01:47:39 +0900

    初コミット　TailwindCSSの設定完了

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <link href="../dist/output.css" rel="stylesheet">
+</head>
+
+<h1 class="text-2xl text-red-500">Hello</h1>
+<h2 class="text-7xl">Hello</h2>
+<body>
+    <span class="material-icons-outlined">
+        login
+    </span>
+
+
+</body>
+
+</html>
