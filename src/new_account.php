7ecbb8ec372994f933dcc5a194f5a3e3dc77afbe
Hayato <hayato.314.nico@gmail.com>
2023-02-02 02:05:08 +0900

    new_account_actに遷移をする機能

--- src/new_account.php
+++ src/new_account.php
@@ -16,26 +16,25 @@ if (isset($_FILES) && !empty($_FILES)) {
 
     $save_filename = date('YmdHis') . $filename;
     // var_dump($save_filename );
-    
+
     $err_msgs = array();
-    $save_path = $upload_dir . $save_filename; 
+    $save_path = $upload_dir . $save_filename;
 
 
 
     if (is_uploaded_file($tmp_path)) {
-        
+
         if (move_uploaded_file($tmp_path, $save_path)) {
             echo $save_filename . 'を' . $upload_dir . 'にアップしました。';
             //DBに保存(ファイル名、ファイルが存在するファイルパス、キャプション)
             $result = fileSave($save_filename, $save_path, 1);
         } else {
-            echo '<br>$tmp_path＝'.$tmp_path . 'を<br>' .'$save_path='. $save_path . 'ファイルの移動に失敗しました。';
+            echo '<br>$tmp_path＝' . $tmp_path . 'を<br>' . '$save_path=' . $save_path . 'ファイルの移動に失敗しました。';
         }
     } else {
         echo 'ファイルが選択されていません。';
         echo '<br>';
     }
-
 }
 
 
@@ -64,6 +63,7 @@ $images = aaa();
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -74,55 +74,62 @@ $images = aaa();
     <script src="https://kit.fontawesome.com/092628cd4c.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/navigation.css">
 </head>
+
 <body>
-<div class="navigation">
-    <nav >
-        <div class="px-8 mx-auto max-w-7xl">
-            <div class="flex items-center justify-between h-16">
-                <div class=" flex items-center">
-                    <i class="fa-solid fa-door-open fa-2xl"></i>
-                </div>
-                <div class="flex">
-                    <h1>HouseHouse</h1>
-                </div>
-                <a href="login.php">
-                    <div class="block">
-                        <div class="flex items-center ml-4 md:ml-6">
-                            <div class="relative ml-3">
-                                <div class="relative inline-block text-left">
-                                    <div >
-                                        <button type="button" class="flex items-center justify-center w-full rounded-md  px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-50 hover:bg-gray-50 dark:hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500" id="options-menu" >
-                                        <i class="fa-solid fa-arrow-right-from-bracket fa-2xl"></i>
-                                        </button>
+    <div class="navigation">
+        <nav>
+            <div class="px-8 mx-auto max-w-7xl">
+                <div class="flex items-center justify-between h-16">
+                    <div class=" flex items-center">
+                        <i class="fa-solid fa-door-open fa-2xl"></i>
+                    </div>
+                    <div class="flex">
+                        <h1>HouseHouse</h1>
+                    </div>
+                    <a href="login.php">
+                        <div class="block">
+                            <div class="flex items-center ml-4 md:ml-6">
+                                <div class="relative ml-3">
+                                    <div class="relative inline-block text-left">
+                                        <div>
+                                            <button type="button" class="flex items-center justify-center w-full rounded-md  px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-50 hover:bg-gray-50 dark:hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500" id="options-menu">
+                                                <i class="fa-solid fa-arrow-right-from-bracket fa-2xl"></i>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </a>
+                    </a>
+                </div>
             </div>
-        </div>
-    </nav>
-</div>
-<!-- ナビバー　終了 -->
+        </nav>
+    </div>
+    <!-- ナビバー　終了 -->
+    <div class="m-10">
 
-<form action="new_account_act.php" method="POST">
-    <fieldset>
-      <legend>ユーザ登録画面</legend>
-      <div>
-        username: <input type="text" name="username">
-      </div>
-      <div>
-        password: <input type="text" name="password">
-      </div>
-      <div>
-        <button>Register</button>
-      </div>
-      <a href="todo_login.php">or login</a>
-    </fieldset>
-</form>
+        <form action="new_account_act.php" method="POST">
+            <fieldset>
+                <legend>ユーザ登録画面</legend>
+                <div>
+                    username: <input type="text" name="username">
+                </div>
+                <div>
+                    e-mail: <input type="text" name="email">
+                </div>
+                <div>
+                    password: <input type="text" name="password">
+                </div>
+                <div>
+                    
+                </div>
+                <a href="new_account_act.php"><button>Register</button></a>
+            </fieldset>
+        </form>
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="js/login-form.js"></script>
 </body>
+
 </html>
